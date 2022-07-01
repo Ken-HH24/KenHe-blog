9ab52d6c9b73c278780ac90d75db5e67df945579
Ken-HH24 <1098862703@qq.com>
2022-07-01 14:36:16 +0800

    feat: rename

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home({
         <div className="flex flex-col-reverse sm:flex-row items-start">
           <div className="flex flex-col pr-8">
             <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
-              Alex He
+              Ken He
             </h1>
             {/* <h2 className="text-gray-700 dark:text-gray-200 mb-4">
               A fontend developer
@@ -55,7 +55,7 @@ export default function Home({
           </div>
           <div className="w-[80px] sm:w-[190px] relative mb-8 sm:mb-0 mr-auto">
             <Image
-              alt="Alex He"
+              alt="Ken He"
               src="/avatar.PNG"
               width={190}
               height={190}
